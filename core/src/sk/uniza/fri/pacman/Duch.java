599c485fab429c4b2f0e2e58dc8ba74d3024a89d
n0rthking <n0rthking@email.cz>
2022-05-18 15:00:40 +0200

    better ghost movement

--- core/src/sk/uniza/fri/pacman/Duch.java
+++ core/src/sk/uniza/fri/pacman/Duch.java
@@ -8,7 +8,8 @@ import java.util.Random;
 public class Duch extends Policko {
     private float pocitadlo;
     private final ArrayList<Smer> vsetkySmery;
-    private final Random generator;
+    private Smer predchadzajuciSmer;
+    private final Vector2 pociatocnaPozicia;
 
     public Duch(ManazerTextur manazerTextur, Vector2 pozicia) {
         super(manazerTextur.getTexturaDuch(), pozicia);
@@ -17,7 +18,63 @@ public class Duch extends Policko {
         this.vsetkySmery.add(Smer.DOLE);
         this.vsetkySmery.add(Smer.VLAVO);
         this.vsetkySmery.add(Smer.VPRAVO);
-        this.generator = new Random();
+        this.pociatocnaPozicia = pozicia.cpy();
+        this.predchadzajuciSmer = null;
+    }
+
+    private Smer dajOpacnySmer(Smer smer) {
+        switch(smer) {
+            case HORE:
+                return Smer.DOLE;
+            case DOLE:
+                return Smer.HORE;
+            case VPRAVO:
+                return Smer.VLAVO;
+            case VLAVO:
+                return Smer.VPRAVO;
+            default:
+                return null;
+        }
+    }
+
+    private ArrayList<Smer> dajMozneSmery(Mapa mapa) {
+        ArrayList<Smer> vysledok = new ArrayList<>();
+
+        for (Smer aktualnySmer : this.vsetkySmery) {
+            Vector2 pozicia = this.getPozicia().cpy().add(aktualnySmer.getPosunX(), aktualnySmer.getPosunY());
+            if (!mapa.getPolicko(pozicia).jePrekazka()) {
+                if (this.predchadzajuciSmer == null) {
+                    vysledok.add(aktualnySmer);
+                } else {
+                    if (this.dajOpacnySmer(aktualnySmer) != this.predchadzajuciSmer) {
+                        vysledok.add(aktualnySmer);
+                    }
+                }
+            }
+        }
+
+        return vysledok;
+    }
+
+    private Smer najdiNajlepsiSmer(ArrayList<Smer> mozneSmery, Hrac hrac) {
+        Smer vysledok = null;
+        float minimum = -1;
+
+        if (mozneSmery.isEmpty()) {
+            System.out.println("prazdne moznosti");
+            return null;
+        }
+
+        for (Smer aktualnySmer : mozneSmery) {
+            Vector2 pozicia = this.getPozicia().cpy().add(aktualnySmer.getPosunX(), aktualnySmer.getPosunY());
+            float rozdiel = hrac.getPozicia().cpy().sub(pozicia).len2();
+            if (minimum == -1 || rozdiel < minimum) {
+                minimum = rozdiel;
+                vysledok = aktualnySmer;
+            }
+        }
+
+        return vysledok;
     }
 
     public void pohniSa(float delta, Mapa mapa, Hrac hrac) {
@@ -26,24 +83,21 @@ public class Duch extends Policko {
         }
 
         this.pocitadlo += delta;
-        if (this.pocitadlo < 0.25) {
+        if (this.pocitadlo < 0.35) {
             return;
         }
         this.pocitadlo = 0;
 
-        Smer smer = Smer.VPRAVO;
-        Vector2 novaPozicia = new Vector2(this.getPozicia().cpy().add(smer.getPosunX(), smer.getPosunY()));
-        if (!mapa.getPolicko(novaPozicia).jePrekazka()) {
-            this.setPozicia(this.getPozicia().cpy().add(smer.getPosunX(), smer.getPosunY()));
-        } else {
-            ArrayList<Smer> mozneSmery = new ArrayList<>();
-            for (Smer aktualnySmer : this.vsetkySmery) {
-                if (!mapa.getPolicko(this.getPozicia().cpy().add(aktualnySmer.getPosunX(), aktualnySmer.getPosunY())).jePrekazka()) {
-                    mozneSmery.add(aktualnySmer);
-                }
-            }
-            smer = mozneSmery.get(generator.nextInt(mozneSmery.size()));
-            this.setPozicia(this.getPozicia().cpy().add(smer.getPosunX(), smer.getPosunY()));
+        ArrayList<Smer> mozneSmery = this.dajMozneSmery(mapa);
+        Smer vybranySmer = this.najdiNajlepsiSmer(mozneSmery, hrac);
+
+        if (vybranySmer == null) {
+            this.setPozicia(this.pociatocnaPozicia.cpy());
+            this.predchadzajuciSmer = null;
+            return;
         }
+
+        this.predchadzajuciSmer = vybranySmer;
+        this.setPozicia(this.getPozicia().cpy().add(vybranySmer.getPosunX(), vybranySmer.getPosunY()));
     }
 }
